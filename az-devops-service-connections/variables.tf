cb0294be6e5c0897000f71b6ddc39a173b92dec4
Marc Rufer <marc.rufer@isolutions.ch>
2024-04-17 08:29:22 +0200

    Add variables file

---
+++ az-devops-service-connections/variables.tf
@@ -0,0 +1,19 @@
+variable "organization_url" {
+  type        = string
+  description = "URL of Azure DevOps organization"
+}
+
+variable "subscription_name" {
+  type        = string
+  description = "Azure subscription name"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Microsoft Entra ID tenant ID"
+}
